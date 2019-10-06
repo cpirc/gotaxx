171f19c7bf116426d191501e842e9d4ff8428fdb
kz04px <6536046+kz04px@users.noreply.github.com>
2019-10-06 20:20:46 +0100

    Add NewMove

--- libs/ataxx/move.go
+++ libs/ataxx/move.go
@@ -1,11 +1,33 @@
 package ataxx
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Move ...
 type Move struct {
 	from Square
 	to   Square
+}
+
+// NewMove ...
+func NewMove(movestr string) (*Move, error) {
+	if len(movestr) == 2 {
+		f := uint8(movestr[0] - '1')
+		r := uint8(movestr[1] - '1')
+		to := Square{r*7 + f}
+		return &Move{to, to}, nil
+	} else if len(movestr) == 4 {
+		f1 := uint8(movestr[0] - '1')
+		r1 := uint8(movestr[1] - '1')
+		fr := Square{r1*7 + f1}
+		f2 := uint8(movestr[3] - '1')
+		r2 := uint8(movestr[4] - '1')
+		to := Square{r2*7 + f2}
+		return &Move{fr, to}, nil
+	}
+	return nil, errors.New("Failed to parse move string")
 }
 
 // IsSingle ...
