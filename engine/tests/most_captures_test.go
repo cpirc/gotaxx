00be1b8e015abc457bb6f3c367cee08fafe72e80
kz04px <6536046+kz04px@users.noreply.github.com>
2019-10-07 12:32:29 +0100

    Add engine tests

---
+++ engine/tests/most_captures_test.go
@@ -0,0 +1,48 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/cpirc/gotaxx/engine/search"
+	"github.com/cpirc/gotaxx/libs/ataxx"
+)
+
+type mostCapturesTest struct {
+	fen   string
+	moves []string
+}
+
+// TestMostCaptures ...
+func TestMostCaptures(t *testing.T) {
+	tests := []mostCapturesTest{
+		{"7/7/7/7/7/7/7 x 0", []string{"0000"}},
+		{"7/7/7/7/7/7/7 o 0", []string{"0000"}},
+		{"7/7/7/7/ooooooo/ooooooo/xxxxxxx x 0", []string{"0000"}},
+		{"7/7/7/7/xxxxxxx/xxxxxxx/ooooooo o 0", []string{"0000"}},
+		{"7/7/7/7/xxxxxxx/xxxxxxx/ooooooo x 0", []string{"a4", "b4", "c4", "d4", "e4", "f4", "g4"}},
+		{"7/7/7/7/ooooooo/ooooooo/xxxxxxx o 0", []string{"a4", "b4", "c4", "d4", "e4", "f4", "g4"}},
+		{"x5o/7/7/7/7/7/o5x x 0", []string{"f1", "f2", "g2", "a6", "b6", "b7"}},
+		{"x5o/7/7/7/7/7/o5x o 0", []string{"b1", "b2", "a2", "g6", "f6", "f7"}},
+		{"x5o/7/7/7/7/7/3o2x x 0", []string{"g1e1", "g1e2", "f1", "f2", "g2", "a6", "b6", "b7"}},
+		{"x5o/7/7/7/7/3o3/3o2x x 0", []string{"g1e1", "g1e2"}},
+	}
+
+	for _, test := range tests {
+		pos, _ := ataxx.NewPosition(test.fen)
+		for i := 0; i < 50; i++ {
+			move := search.MostCaptures(*pos)
+			found := false
+
+			for _, movestr := range test.moves {
+				if move.String() == movestr {
+					found = true
+					break
+				}
+			}
+
+			if found == false {
+				t.Errorf("Position %s wrong move %s", test.fen, move)
+			}
+		}
+	}
+}
---
+++ engine/tests/random_test.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/cpirc/gotaxx/engine/search"
+	"github.com/cpirc/gotaxx/libs/ataxx"
+)
+
+type randomTest struct {
+	fen   string
+	moves []string
+}
+
+// TestRandom ...
+func TestRandom(t *testing.T) {
+	tests := []randomTest{
+		{"7/7/7/7/7/7/7 x 0", []string{"0000"}},
+		{"7/7/7/7/7/7/7 o 0", []string{"0000"}},
+		{"7/7/7/7/ooooooo/ooooooo/xxxxxxx x 0", []string{"0000"}},
+		{"7/7/7/7/xxxxxxx/xxxxxxx/ooooooo o 0", []string{"0000"}},
+		{"o6/7/7/7/7/7/6x x 0", []string{"f1", "f2", "g2", "g1e1", "g1e2", "g1e3", "g1f3", "g1g3"}},
+		{"x6/7/7/7/7/7/6o o 0", []string{"f1", "f2", "g2", "g1e1", "g1e2", "g1e3", "g1f3", "g1g3"}},
+	}
+
+	for _, test := range tests {
+		pos, _ := ataxx.NewPosition(test.fen)
+		for i := 0; i < 50; i++ {
+			move := search.Random(*pos)
+			found := false
+
+			for _, movestr := range test.moves {
+				if move.String() == movestr {
+					found = true
+					break
+				}
+			}
+
+			if found == false {
+				t.Errorf("Position %s incorrect move %s", test.fen, move)
+			}
+		}
+	}
+}
