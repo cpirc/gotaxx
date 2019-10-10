fa33ed459d548586bcbbf1e196f4e5aa7a8e651b
kz04px <6536046+kz04px@users.noreply.github.com>
2019-10-10 20:58:35 +0100

    Eval fix
    score is already from the side to move's point of view

--- engine/eval/eval.go
+++ engine/eval/eval.go
@@ -4,9 +4,5 @@ import "github.com/cpirc/gotaxx/libs/ataxx"
 
 func Eval(pos *ataxx.Position) int {
 	score := pos.Us().Count() - pos.Them().Count()
-
-	if pos.Turn() == 0 {
-		return score
-	}
-	return -score
+	return score
 }
