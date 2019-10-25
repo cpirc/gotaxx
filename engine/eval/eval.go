c9da3b27d882ffc9e774b62b3997157c4f854490
Manik Charan <mkchan2951@gmail.com>
2019-10-26 00:50:18 +0530

    Add psqt eval

--- engine/eval/eval.go
+++ engine/eval/eval.go
@@ -2,7 +2,31 @@ package eval
 
 import "github.com/cpirc/gotaxx/libs/ataxx"
 
+var MATERIAL = 1000
+
+var PST = []int{
+	30, 20, 10, 10, 10, 20, 30, 20, 10, 10, 5, 10, 10,
+	20, 10, 10, 5, 0, 5, 10, 10, 10, 5, 0, 0, 0,
+	5, 10, 10, 10, 5, 0, 5, 10, 10, 20, 10, 10, 5,
+	10, 10, 20, 30, 20, 10, 10, 10, 20, 30,
+}
+
 func Eval(pos *ataxx.Position) int {
-	score := pos.Us().Count() - pos.Them().Count()
+	score := 0
+
+	colors := []ataxx.Bitboard{pos.Us(), pos.Them()}
+	for _, bb := range colors {
+		score += bb.Count() * MATERIAL
+
+		for bb.Data != 0 {
+			sq := bb.LSB()
+			bb.Data ^= uint64(1) << sq
+
+			score += PST[sq]
+		}
+
+		score = -score
+	}
+
 	return score
 }
